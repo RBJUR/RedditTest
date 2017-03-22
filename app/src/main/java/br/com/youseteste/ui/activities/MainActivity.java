b168431a2c394fa6c5eac819d57fdbc640150b5a
Roque Buarque <roque.buarque@gmail.com>
2017-03-22 08:53:24 -0300

    fragments(WIP)

--- app/src/main/java/br/com/youseteste/ui/activities/MainActivity.java
+++ app/src/main/java/br/com/youseteste/ui/activities/MainActivity.java
@@ -1,9 +1,12 @@
 package br.com.youseteste.ui.activities;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import br.com.api.response.PostListResponse;
@@ -11,10 +14,12 @@ import br.com.youseteste.R;
 import br.com.youseteste.adapter.PostListAdapter;
 import br.com.youseteste.application.App;
 import br.com.youseteste.presenter.PostListPresenter;
+import br.com.youseteste.ui.fragments.PostListFragment;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
-
+    private Fragment mFragment;
+    private FrameLayout mFrameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +33,14 @@ public class MainActivity extends AppCompatActivity  {
     }
 
     private void bindViews() {
-        
+        mFrameLayout = (FrameLayout) findViewById(R.id.main_fragment);
     }
 
     private void replaceListFragment() {
+        FragmentManager manager = getSupportFragmentManager();
+        mFragment = PostListFragment.newInstance();
+        if (mFragment != null)
+            manager.beginTransaction().replace(R.id.main_fragment, mFragment).commit();
 
     }
 
