35d5654197ba5be60686454c2fabc70c5d447dbe
Roque Buarque <roque.buarque@gmail.com>
2017-03-26 09:53:52 -0300

    toolbar

---
+++ app/src/main/java/br/com/youseteste/helper/ToolbarHelper.java
@@ -0,0 +1,54 @@
+package br.com.youseteste.helper;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import br.com.youseteste.R;
+
+public class ToolbarHelper {
+
+    public static final int WHITE = 0xffffffff;
+
+    public static Toolbar setup(@NonNull final AppCompatActivity activity, Toolbar toolbar, boolean backPressedListener,
+                                boolean displayHomeAsUpEnabled, @NonNull String title) {
+        if (toolbar != null) {
+            activity.setSupportActionBar(toolbar);
+            assert activity.getSupportActionBar() != null;
+            activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+            if (displayHomeAsUpEnabled)
+                activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            if (backPressedListener)
+                toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        activity.onBackPressed();
+                    }
+
+                });
+
+            TextView toolbarTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
+
+            if (toolbarTitle != null) {
+                toolbarTitle.setMaxLines(1);
+                toolbarTitle.setEllipsize(TextUtils.TruncateAt.END);
+                toolbarTitle.setText(title);
+
+                //toolbarTitle.setGravity(Gravity.CENTER);
+
+                Toolbar.LayoutParams params = new Toolbar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                toolbarTitle.setLayoutParams(params);
+            }
+
+            toolbar.setBackgroundColor(WHITE);
+        }
+        return toolbar;
+    }
+}
--- app/src/main/java/br/com/youseteste/ui/fragments/PostListFragment.java
+++ app/src/main/java/br/com/youseteste/ui/fragments/PostListFragment.java
@@ -1,10 +1,14 @@
 package br.com.youseteste.ui.fragments;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +18,7 @@ import br.com.api.response.ChildrenResponse;
 import br.com.api.response.PostListResponse;
 import br.com.youseteste.R;
 import br.com.youseteste.adapter.PostListAdapter;
+import br.com.youseteste.helper.ToolbarHelper;
 import br.com.youseteste.presenter.PostListPresenter;
 import br.com.youseteste.ui.activities.MainActivity;
 import jp.wasabeef.recyclerview.animators.SlideInUpAnimator;
@@ -28,20 +33,63 @@ public class PostListFragment extends Fragment implements PostListPresenter.Post
     private PostListAdapter mAdapter;
     private RecyclerView mRecyclerView;
 
+    private int bottomNavigationY;
+
+    private BottomNavigationView bottomNavigationView;
+
     private SwipeRefreshLayout swipeRefreshLayout;
 
     private PostListResponse postListResponse;
+
+    private Toolbar toolbar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_post_list, container, false);
 
         bindViews(rootView);
-
+        setupToolbar();
         initPresenter();
         doRequestPostList();
 
+        recycleViewScrollListener();
+
+
         return rootView;
+    }
+
+    private void setupToolbar() {
+        ToolbarHelper.setup((AppCompatActivity) getActivity(), toolbar, false, false, getResources().getString(R.string.app_name));
+
+    }
+
+    private void recycleViewScrollListener() {
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                bottomNavigationAnimation(false);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (dy < 0) {
+                    bottomNavigationAnimation(false);
+                } else if (dy > 0) {
+                    bottomNavigationAnimation(true);
+                }
+            }
+        });
+    }
+
+    private void bottomNavigationAnimation(boolean start) {
+        if (start) {
+            bottomNavigationView.animate().setDuration(50).translationY(bottomNavigationY + bottomNavigationView.getHeight() * 4).start();
+        } else {
+            bottomNavigationView.animate().setDuration(1000).translationY(0).start();
+
+        }
     }
 
     public static Fragment newInstance() {
@@ -50,10 +98,14 @@ public class PostListFragment extends Fragment implements PostListPresenter.Post
     }
 
     private void bindViews(View view) {
+        toolbar = (Toolbar) view.findViewById(R.id.toolbar);
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
         mRecyclerView.setHasFixedSize(true);
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(getContext());
         mRecyclerView.setLayoutManager(mLayoutManager);
+
+        bottomNavigationView = (BottomNavigationView) view.findViewById(R.id.bottom_navigation);
+        bottomNavigationY = (int) bottomNavigationView.getTranslationY();
 
         swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh_layout);
 
@@ -79,6 +131,8 @@ public class PostListFragment extends Fragment implements PostListPresenter.Post
 
         mRecyclerView.setItemAnimator(new SlideInUpAnimator(new OvershootInterpolator(1f)));
         mRecyclerView.getItemAnimator().setAddDuration(600);
+
+        bottomNavigationView.bringToFront();
 
     }
 
@@ -117,7 +171,11 @@ public class PostListFragment extends Fragment implements PostListPresenter.Post
 
             ChildrenResponse itemList = postListResponse.getDataResponse().getChildrenResponse().get(position);
 
-            String url = itemList.getListItemResponse().getPreview().getImages().get(0).getSource().getUrl();
+            String url = "";
+            if (itemList.getListItemResponse().getPreview() != null && itemList.getListItemResponse().getPreview().getImages().size() > 0 &&
+                    itemList.getListItemResponse().getPreview().getImages().get(0).getSource() != null) {
+                url = itemList.getListItemResponse().getPreview().getImages().get(0).getSource().getUrl();
+            }
             bundle.putString("url", url);
 
             postDetailFragment.setArguments(bundle);
